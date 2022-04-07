4be707ea5326af9ec3ec533bac3f2ed79009f2cf
Arkashu <kopeikaarkashu@gmail.com>
2022-04-07 22:02:18 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+function counter(num) {
+    let num2 =0;
+    return function (num) {
+        return num2 = num2 + num;
+    }
+}
+
+let newCounter1 = counter();
+
+console.log(newCounter1(2))
+console.log(newCounter1(5))
+console.log(newCounter1(10))
